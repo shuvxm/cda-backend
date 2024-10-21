448abf07d6ec7f97493415f1eb90ba7890527277
shuvxm <kumarshubham5559@gmail.com>
2024-10-21 07:50:04 +0530

    ""

--- src/main/java/org/jsp/cda/service/EnrollmentService.java
+++ src/main/java/org/jsp/cda/service/EnrollmentService.java
@@ -5,4 +5,10 @@ import org.springframework.http.ResponseEntity;
 public interface EnrollmentService {
 
     ResponseEntity<?> saveEnrollment(int cid, int sid);
+
+    ResponseEntity<?> findEnrollmentById(int id);
+
+    ResponseEntity<?> findAllEnrollments();
+
+
 }
