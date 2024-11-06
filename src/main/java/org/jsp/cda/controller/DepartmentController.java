a932b5ad89f68fcc6be0f0ed1077ab31960e55b6
shuvxm <kumarshubham5559@gmail.com>
2024-11-06 19:45:13 +0530

    "API documentation"

--- src/main/java/org/jsp/cda/controller/DepartmentController.java
+++ src/main/java/org/jsp/cda/controller/DepartmentController.java
@@ -1,5 +1,8 @@
 package org.jsp.cda.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.jsp.cda.entity.Department;
 import org.jsp.cda.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,13 +17,22 @@ public class DepartmentController
 	@Autowired
 	private DepartmentService service;
 
-
+	@Operation(summary = "To Create the Department ", description = "This API will accept the request body of department Entity ")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Department created successfully"),
+			@ApiResponse(responseCode = "400", description = "Invalid input data")
+	} )
 	@PostMapping
 	public ResponseEntity<?> saveDepartment(@RequestBody Department department)
 	{
 		return service.saveDepartment(department);
 	}
 
+	@Operation(summary = "To find all the Department ", description = "This API will fetched all the department ")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Department fetched successfully"),
+			@ApiResponse(responseCode = "400", description = "Invalid input data")
+	} )
 	@GetMapping
 	public ResponseEntity<?> findAllDepartments()
 	{
