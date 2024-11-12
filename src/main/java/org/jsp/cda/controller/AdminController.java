5e6e81951a5d27297e9c86e4d818bf355b00b9ac
shuvxm <kumarshubham5559@gmail.com>
2024-11-12 18:57:38 +0530

    "API documentation"

--- src/main/java/org/jsp/cda/controller/AdminController.java
+++ src/main/java/org/jsp/cda/controller/AdminController.java
@@ -1,5 +1,8 @@
 package org.jsp.cda.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.jsp.cda.entity.Administrator;
 import org.jsp.cda.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +17,11 @@ public class AdminController {
     @Autowired
     private AdminService adminService;
 
+    @Operation(summary = "To Create the admin ", description = "This API will accept the request body of Admin Entity ")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Course created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data")
+    })
     @PostMapping
     public ResponseEntity<?> saveAdmin(@RequestBody Administrator administrator){
         return adminService.saveAdmin(administrator);
